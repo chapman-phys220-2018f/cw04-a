9e8c8fa0f8bb88bb45cf47b8e7f4d1eeeba3a988
wheatonabigail <wheaton@chapman.edu>
2018-09-20 18:01:33 +0000

    primes

--- primes.py
+++ primes.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python3
 
-# Royal Cuevas and Abigail Wheaton 
+#Royal Cuevas and Abigail Wheaton 
 #2285562 and 2299246 
 #cueva114@mail.chapman.edu and wheaton@chapman.edu 
 #PHYS220 Fall 2018
-# CW 04
+#CW 04
 
 import math
 
+
 def eratosthenes(n):
-    integers=[]
-    primes=[]
-    j=1
-    for i in range(2,n):
-        integers.append(i)
-    while integers[0]<=math.sqrt(n):
-        primes.append(integers[0])
-        for x in integers:
-            if (integers[j] % integers[0]) == 0:
-                del integers[j]
-            j=j+1
-    for x in integers:
+    primes = []
+    for x in range(2, n+1):
+        if all(x % i != 0 for i in range(2, x)):
             primes.append(x)
     return primes
