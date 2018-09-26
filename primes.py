57bea7ef51b50b3e4381284952fb45914630318a
wheatonabigail <wheaton@chapman.edu>
2018-09-26 18:21:39 +0000

    test

--- primes.py
+++ primes.py
@@ -15,3 +15,22 @@ def eratosthenes(n):
         if all(x % i != 0 for i in range(2, x)):
             primes.append(x)
     return primes
+
+def gen_eratosthenes():
+    index = 0
+    count = 0
+    list_primes = [2]
+    next_prime = 3
+    yield list_primes[index]
+    while (True):
+        for i in range(2, next_prime):
+            if (next_prime %I != 0):
+                count += 1
+            else:
+                break
+            if (count == next_prime - 2):
+                index += 1
+                list_primes.append(next_prime)
+                yield list_primes[index]
+        count = 0
+        next_prime += 1
